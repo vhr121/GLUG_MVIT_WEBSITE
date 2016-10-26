47c1e4bb6f85b5d626f957bc5a613fae37e47ab4
vhr121 <veereshhr121@gmail.com>
2016-10-26 20:51:43 +0530

    some tweaks

--- static/js/jscript.js
+++ static/js/jscript.js
@@ -15,5 +15,5 @@ function showSlides() {
     }
     slides[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 3000);
+    setTimeout(showSlides, 5000);
 }
